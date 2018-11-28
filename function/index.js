b370d4c12be163b06e3bb7dfc10bfad41fe3a373
Ian MacGregor <ijmacgregor@gmail.com>
2018-11-28 20:16:00 +1100

    code cleanup

--- function/index.js
+++ function/index.js
@@ -1,65 +1,70 @@
 const fs = require("fs");
 const MongoClient = require("mongodb").MongoClient;
 
-async function getConnection(){
-    // Get mongodb user & pass from secrets
-    const username = fs.readFileSync('/secrets/mongodb-credentials/username', 'utf8');
-    const password = fs.readFileSync('/secrets/mongodb-credentials/password', 'utf8');
-    
-    // Construct uri
-    const uri = "mongodb+srv://" + username + ":" + password  + "@faasdemo-uiuxc.mongodb.net/faasdemodb?retryWrites=true";
-    
-    // Get mongodb connection
-    return MongoClient.connect(uri, { useNewUrlParser: true });
-}
+const MongoUtil = { 
+	getConnection : async () => {
+	    // Get mongodb user & pass from secrets
+	    const username = fs.readFileSync('/secrets/mongodb-credentials/username', 'utf8'),
+	    	  password = fs.readFileSync('/secrets/mongodb-credentials/password', 'utf8');
+	    
+	    // Construct uri
+	    const uri = "mongodb+srv://" + username + ":" + password  + "@faasdemo-uiuxc.mongodb.net/faasdemodb?retryWrites=true";
+	    
+	    // Get mongodb connection
+	    return MongoClient.connect(uri, { useNewUrlParser: true });
+	},
+	
+	// Gets the products collection in DB faasdemodb
+	getCollection: client => client.db("faasdemodb").collection("products"),
+	
+	// Creates a product in collection products
+	createProduct: async (product) => {
+	    return await MongoUtil.getConnection().then(async client => {
+	        return await MongoUtil.getCollection(client).insertOne(product);
+	    });
+	},
+	
+	// Gets all records in collection products
+	getProducts: async () => {
+	    return await MongoUtil.getConnection().then(async client => {
+	        return await MongoUtil.getCollection(client).find({}).toArray();
+	    });
+	}
+};
 
-function getProductCollection(client){
-    return client.db("faasdemodb").collection("products");   
-}
-
-async function createProduct(client, product){
-    return getProductCollection(client).insertOne(product);
-}
-
-async function getAllProducts(client){
-    return getProductCollection(client).find({}).toArray();
-}
-
-
-module.exports.handler = async (event, context) => { 
-    var req = event.extensions.request;
-    var res = event.extensions.response;
+module.exports.handler = async (event, context) => {  
+    var request = event.extensions.request;
+    var response = event.extensions.response;
     
     // Allow cross origin requests
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST");
     
     // No caching
-    res.setHeader("Cache-Control", "no-cache");
+    response.setHeader("Cache-Control", "no-cache");
     
-    // Get a connection, holding the thread until the promise is resolved
-    await getConnection().then(async function(client){
-        // Connection established, process request according to method
-        if(req.method === "GET") {
-            // Get all products
-            await getAllProducts(client).then(function(aProducts){
-                res.status(200).json(aProducts); 
-            });
-        } else if(req.method === "POST"){
-            // Create a product
-            await createProduct(client, JSON.parse(req.body.toString())).then(function(response){
-                res.status(201).json({
-                    productId: response.insertedId
-                });
-            });
-        } else {
-            // Other methods not implemented
-            res.status(501).json({
-                errorMessage: "Method not implemented"
-            });
-        }
-    }).catch(function(err){
-        // Error occurred
-        res.status(500).send(err);
-    });
+    // Process depending on request method
+    switch(request.method){
+    	case "POST":
+    		// Create product 
+    		await MongoUtil.createProduct(JSON.parse(request.body.toString())).then(data => {
+    			// Product created, return 201 success code and the created record id
+    			response.status(201).json({
+    				productId: data.insertedId
+    			});
+    		});
+    		break;
+    	case "GET":
+    		// Get all products
+    	    await MongoUtil.getProducts().then(products => {
+    	    	// Products retrieved, return 200 success code and the json product array
+    	        response.status(200).json(products);
+    	    });
+    		break;
+    	default:
+    		// Invalid method, return error
+    		response.status(501).json({
+    			errorMessage: "Method not implemented"
+    		});
+    }
 }
